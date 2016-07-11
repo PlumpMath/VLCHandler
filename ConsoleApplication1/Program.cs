7103b14e9c4cd716997fb51f73add2bb2edc22ea
stonfute <preaultc@gmail.com>
2016-07-11 18:38:31 +0200

    first commit ajout sources

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Win32;
+
+
+namespace VLCHandler
+{
+    class Program
+    {
+        static string FindByDisplayName(RegistryKey parentKey, string name)
+        {
+            string[] nameList = parentKey.GetSubKeyNames();
+            for (int i = 0; i < nameList.Length; i++)
+            {
+                RegistryKey regKey = parentKey.OpenSubKey(nameList[i]);
+                try
+                {
+                    if (regKey.GetValue("DisplayName").ToString() == name)
+                    {
+                        return regKey.GetValue("DisplayIcon").ToString();
+                    }
+                }
+                catch { }
+            }
+            return "";
+        }
+        static string processFile(string arg)
+        {
+            string file = arg.Replace("vlchandler:","");
+            return file;
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            RegistryKey regKey = Registry.LocalMachine.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Uninstall");
+            string vlc_path = FindByDisplayName(regKey, "VLC media player");
+            string video_file = "";
+
+            foreach (string s in args)
+            {
+                video_file = processFile(s);
+            }
+
+            System.Diagnostics.Process.Start(vlc_path, video_file);
+ 
+        }
+    }
+}
